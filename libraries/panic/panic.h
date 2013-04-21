4fd79183c45d95fbcfc935e5f9ecf4b4787bc2ee
Steven Pearson <steven.pearson.78@googlemail.com>
2013-04-21 11:58:22 +0200

    Added BSD style license

--- libraries/panic/panic.h
+++ libraries/panic/panic.h
@@ -1,5 +1,8 @@
 #ifndef PANIC_H
 #define PANIC_H
+// Copyright 2013 Steven Pearson
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
 
 typedef unsigned char paniccode;
 
